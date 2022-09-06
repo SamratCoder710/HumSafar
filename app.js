6050770dedda8281112ddb4665a3862810261d47
unknown <shubhamksrathod26@gmail.com>
2022-09-07 01:13:32 +0530

    db change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const mongoStore = require('connect-mongo');
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 
 
